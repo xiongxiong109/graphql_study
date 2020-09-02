d0b146bc1d0385022896d95bb455bea3f27d44ae
jqxiong <jqxiong@trip.com>
2020-09-02 17:29:11 +0800

    graphql init

---
+++ index.js
@@ -0,0 +1,66 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+// 定义schema
+const typeDefs = gql`
+
+# 定义类型
+type Book {
+    title: String
+    author: Author
+}
+
+# 定义作者纬度
+type Author {
+    # 作者姓名
+    name: String
+    # 作者的书籍
+    books: [Book]
+}
+
+# 定义查询类型
+type Query {
+    books: [Book]
+    authors: [Author]
+}
+`
+
+// const author = {
+//     name: 'jqxiong',
+//     books: [ book1, book2 ]
+// }
+
+const book1 = {
+    title: 'asdasd',
+    author: {
+        name: 'jqxiong',
+        books: []
+    }
+}
+
+const books = [
+    book1
+]
+
+const authors = [
+    {
+        name: 'jqxiong',
+        books: [
+            {
+                title: 'asasas'
+            }
+        ]
+    }
+]
+
+const resolvers = {
+    Query: {
+        books: () => books,
+        authors: () => authors
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+    console.log(url)
+})
