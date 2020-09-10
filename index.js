b54f04e50061cb018c03e3d9fa97db6b9d9007eb
jqxiong <jqxiong@trip.com>
2020-09-10 16:29:02 +0800

    query带参数

--- index.js
+++ index.js
@@ -3,27 +3,61 @@ const { GraphQLScalarType } = require('graphql')
 
 // 定义schema
 const typeDefs = gql`
-type User {
-    id: Int!
+
+# 性别枚举
+enum Gender {
+    MALE
+    FEMALE
+}
+
+type Skill {
     name: String
-    age: Int
+    func: String
 }
+
+type Human {
+    # 性别
+    gender: Gender
+    # 姓名
+    name: String!
+    # 技能
+    skills: [Skill]
+}
+
 type Query {
-    users: [User]
+    # 根据姓名模糊查询
+    human(fuzzyName: String, gender: Gender): [Human]
 }
+
 `
+
+const users = [
+    {
+        name: 'xx'
+    },
+    {
+        name: 'xxa',
+        gender: 'MALE'
+    },
+    {
+        name: 'aa'
+    },
+    {
+        name: 'bol'
+    }
+]
 
 // 具体的实现都在resolver中
 const resolvers = {
     Query: {
-        users: () => {
-            return [
-                {
-                    id: 1,
-                    name: 'aa',
-                    age: 23
-                }
-            ]
+        // args为传入的参数
+        human: (obj, args, context, info) => {
+            const { fuzzyName, gender } = args
+            if (fuzzyName) {
+                const reg = new RegExp(fuzzyName, 'i')
+                return users.filter(item => reg.test(item.name) && (item.gender == gender || !gender))
+            }
+            return users
         }
     }
 }
